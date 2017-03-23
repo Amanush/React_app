9546fb1f1a7a1f97baa31baccbc9518a4d9fecb6
mikeal888 <michael.kewming@gmail.com>
2017-03-23 20:44:46 +1100

    Created header file, css

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Heading from './Header/title';
+
+class Header extends Component {
+
+	render() {
+		return (
+			<div className="App-header">
+				<Heading/>
+			</div>
+		);
+	};
+};
+
+export default Header;
---
+++ src/Header/title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Heading extends Component {
+
+	render() {
+		return (
+			<div className="Heading">
+				<h1> Michael Kewiming </h1>
+			</div>
+		);
+	};
+};
+
+export default Heading;
